d69f7c9f5480493871c93b9e8516f7ced7186a09
pankajgour12 <pankajgour5000@gmail.com>
2026-04-11 19:47:06 +0530

    Project Setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+  <div className='w-full min-h-screen bg-stone-900 text-amber-50'>
+   <h1>Hola Mundo </h1>
+  </div>
+  )
+}
+
+export default App
