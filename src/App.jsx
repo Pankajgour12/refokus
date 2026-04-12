6cde50553384c89dafef7a2b37ddb7dd6cc19965
pankajgour12 <pankajgour5000@gmail.com>
2026-04-12 10:12:43 +0530

    Work component ✅ [ Animation pending ]

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 
 import './App.css'
 import Navbar from './components/Navbar'
+import Work from './components/Work'
 
 function App() {
 
   return (
-  <div className='w-full min-h-screen bg-stone-900 text-amber-50'>
+  <div className='w-full min-h-screen bg-stone-900 text-white'>
    <Navbar/>
+   <Work/>
   </div>
   )
 }
