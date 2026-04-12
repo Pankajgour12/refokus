c2952783c6caa5a1d3c20e5644d506e2974cf96f
pankajgour12 <pankajgour5000@gmail.com>
2026-04-12 18:48:30 +0530

    Products done [ Animation pending ]

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,11 +1,11 @@
 import { MoveRight } from 'lucide-react'
 import React from 'react'
 
-const Button = () => {
+const Button = ({title = "Get Started"}) => {
   return (
-    <button className='flex items-center gap-2 px-3 sm:px-4 py-2 text-xs sm:text-sm bg-white text-black rounded-full font-medium whitespace-nowrap shrink-0'>
+    <button className='flex items-center gap-2 px-3 sm:px-4 py-2 text-xs sm:text-sm bg-white hover:bg-gray-300 text-black rounded-full font-medium whitespace-nowrap shrink-0'>
       
-      <span className='hidden sm:inline'>Get Started</span>
+      <span className='hidden sm:inline'>{title} </span>
 
       <MoveRight size={16} />
 
--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,13 +1,16 @@
 import Button from "./Button"
 
-const Product = () => {
+const Product = ({data}) => {
   return (
     <div className="w-full py-20 bg-black text-white">
         <div className="max-w-7xl mx-auto flex justify-between items-center">
-            <h1 className="text-6xl capitalize font-semibold">arqitel</h1>
+            <h1 className="text-6xl capitalize font-medium">{data.title}</h1>
             <div className="dets w-1/3 flex flex-col justify-between items-start gap-10">
-                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Adipisci quod ab veritatis, autem aliquam neque beatae ad? Beatae, nam aperiam.</p>
-                <Button/>
+                <p>{data.desc} </p>
+                <div className="flex items-center gap-5">
+                {data.live && <Button/>}
+                {data.case && <Button title="Case Study" />}
+                </div>
             </div>
         </div>
     </div>
