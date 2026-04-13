7484bf1c2aafcdccdf01ca7d95fdb7cebf7e2c03
pankajgour12 <pankajgour5000@gmail.com>
2026-04-13 13:05:22 +0530

    Refactor Footer layout and styles for improved spacing and alignment

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -9,7 +9,7 @@ const Footer = () => {
         </div>
 
         {/* RIGHT */}
-        <div className="flex mt-32 gap-20">
+        <div className="flex mt-32 gap-14 select-none">
 
          
           <div>
@@ -31,11 +31,11 @@ const Footer = () => {
               <li className="hover:text-gray-300 cursor-pointer">Contact</li>
             </ul>
           </div>
-          <div className="flex flex-col gap-5">
-            <p className="max-w-md text-gray-400">
+          <div className="flex flex-col gap-5 items-end">
+            <p className="mt-10 text-gray-400">
         Refokus is pioneering digital agency driven by design and empowered by technology.
       </p>
-      <img className="w-24 bg-blue-500 px-4 py-2" src="https://assets-global.website-files.com/6334198f239547d0f9cd84b3/63349803431f1562dccf1802_refokus%20logo.svg" alt="" />
+      <img className="w-24 bg-blue-500 px-6 py-2" src="https://assets-global.website-files.com/6334198f239547d0f9cd84b3/63349803431f1562dccf1802_refokus%20logo.svg" alt="" />
 
 
           </div>
