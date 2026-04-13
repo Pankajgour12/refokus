e7e495f4e175cacdcf52c80724e8dbec78d6a844
pankajgour12 <pankajgour5000@gmail.com>
2026-04-13 12:58:01 +0530

    Footer done

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
-    <footer className="max-w-7xl mx-auto  px-10 py-12">
-      <div className="flex justify-between items-center gap-20">
+    <footer className="max-w-7xl mx-auto  px-10 py-8">
+      <div className="flex justify-between items-center gap-32">
 
         {/* LEFT */}
         <div className="text-[12rem] font-semibold  tracking-tight select-none">
@@ -9,7 +9,7 @@ const Footer = () => {
         </div>
 
         {/* RIGHT */}
-        <div className="flex gap-20">
+        <div className="flex mt-32 gap-20">
 
          
           <div>
@@ -32,7 +32,7 @@ const Footer = () => {
             </ul>
           </div>
           <div className="flex flex-col gap-5">
-            <p className="mt-10 max-w-md text-gray-400">
+            <p className="max-w-md text-gray-400">
         Refokus is pioneering digital agency driven by design and empowered by technology.
       </p>
       <img className="w-24 bg-blue-500 px-4 py-2" src="https://assets-global.website-files.com/6334198f239547d0f9cd84b3/63349803431f1562dccf1802_refokus%20logo.svg" alt="" />
